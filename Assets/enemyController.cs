94e0d6366ddd7fc5542e10e55effd4373e029866
cebollinos11 <cebollinos@gmail.com>
2016-02-13 15:19:17 +0100

    navmesh test

---
+++ Assets/enemyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
