af3621fa082f1f8dc7e4cb5207fec8b65cc1ef6a
cebollinos11 <cebollinos@gmail.com>
2016-02-15 19:00:04 +0100

    basic map

--- Assets/enemyController.cs
+++ Assets/enemyController.cs
@@ -14,6 +14,8 @@ public class enemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetMouseButtonDown(0)) {
+            agent.SetDestination(target.transform.position);
+        }
 	}
 }
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+
+	NavMeshAgent agent;
+    Vector3 target;
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+        
+	}
+
+    void MoveTo(Vector3 pos) {
+        target = pos;
+        agent.SetDestination(target);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+
+            //Vector3 v3 = Input.mousePosition;
+            //v3.z = 20f;
+            //v3 = Camera.main.ScreenToWorldPoint(v3);
+            //transform.position = v3;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            transform.position = hit.point;
+        }
+        
+	
+	}
+}
