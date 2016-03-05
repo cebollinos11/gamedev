e6a732222b777fb62813886d633e7415a34b4019
cebollinos11 <cebollinos@gmail.com>
2016-03-05 13:26:14 +0100

    change material on digital form

---
+++ Assets/Scripts/TextureHider.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureHider : MonoBehaviour {
+
+
+    Material originalMat;
+    Renderer rend;
+
+    Material hiddenMat;
+
+	// Use this for initialization
+	void Start () {
+        originalMat = GetComponent<Renderer>().material;
+        rend = GetComponent<Renderer>();
+        hiddenMat = Resources.Load("Transparent") as Material;
+	}
+
+    public void Hide() {
+        Debug.Log("hiding");
+        //rend.material.SetFloat("_Mode", 3);
+        //rend.material.color = Color.black;
+        rend.material = hiddenMat;
+    }
+
+    public void Show()
+    {
+        Debug.Log("showing");
+        rend.material = originalMat;
+    }
+
+    
+
+
+	
+}
---
+++ Assets/Scripts/TextureHiderManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureHiderManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    void HidePhysicalWorld() {
+
+        TextureHider[] realwalls = Object.FindObjectsOfType<TextureHider>();
+
+        for (int i = 0; i < realwalls.Length; i++)
+        {
+            realwalls[i].Hide();
+        }
+    
+    }
+
+    void ShowPhysicalWorld()
+    {
+
+        TextureHider[] realwalls = Object.FindObjectsOfType<TextureHider>();
+
+        for (int i = 0; i < realwalls.Length; i++)
+        {
+            realwalls[i].Show();
+        }
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            HidePhysicalWorld();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ShowPhysicalWorld();
+        }
+    }
+}
