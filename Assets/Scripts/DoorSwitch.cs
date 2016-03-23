f81855650ecd6dfe869eb4c171f0f9fbbac05064
Muhsin <muhsinkaymak@hotmail.com>
2016-03-23 14:01:12 +0100

    split

---
+++ Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSwitch : MonoBehaviour {
+
+    GameMaster gamemaster;
+    FormsManager formManager;
+    GameObject[] playerForms;
+    bool splitted = false;
+
+    public Transform door;
+    [SerializeField] bool complete, physical, digital = false;
+    
+
+	// Use this for initialization
+	void Start () {
+        playerForms = GameObject.FindGameObjectsWithTag("Player");
+        gamemaster = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+        formManager = gamemaster.GetComponent<FormsManager>();
+
+        if(complete)
+        {
+            physical = digital = complete;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(gamemaster.curTurn == GameMaster.turns.player)
+        {
+            if(formManager.isSplitted != splitted)
+            {
+                splitted = formManager.isSplitted;
+                playerForms = GameObject.FindGameObjectsWithTag("Player");
+            }
+
+            if (complete)
+            {
+
+            }
+            if(physical)
+            {
+
+            }
+            if(digital)
+            {
+
+            }
+        }
+	}
+}
