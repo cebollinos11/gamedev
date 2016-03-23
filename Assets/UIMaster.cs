baf311d19898c64ce17b266b845333f7a9b12d0c
cebollinos11 <cebollinos@gmail.com>
2016-03-23 17:34:46 +0100

    game over on ui

---
+++ Assets/UIMaster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMaster : MonoBehaviour {
+
+
+    public GameObject GameOver;
+
+	// Use this for initialization
+	void Start () {
+        GameOver.SetActive(false);
+	}
+
+    public void ShowGameOver() {
+
+        GameOver.SetActive(true);
+    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
