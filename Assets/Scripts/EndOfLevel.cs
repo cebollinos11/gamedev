befbd3454e00081dc5b39a80f91051a71a549c09
cebollinos11 <cebollinos@gmail.com>
2016-04-07 14:20:26 +0200

    end of level prefab

--- Assets/Scripts/EndOfLevel.cs
+++ Assets/Scripts/EndOfLevel.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class EndOfLevel : MonoBehaviour {
+    FormsManager fm;
+    public string NextLevelName;
+    bool triggeredAlready;
+    void Start() {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        fm = GameObject.FindObjectOfType<FormsManager>();
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!fm.isSplitted) 
+        {
+            if (!triggeredAlready) {
+                triggeredAlready = true;
+                Debug.Log("SHOW LEVEL COMPLETE!");
+                Invoke("GoToNextLevel", 2f);
+
+            
+            }
+        }
+        
+    }
+
+    void GoToNextLevel()
+    {
+
+        Debug.Log("GO TO NEXT LEVEL");
+        Application.LoadLevel(NextLevelName);
+
+    }
 }
