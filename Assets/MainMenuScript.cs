8789e6f4ddd64a52e7131f7d6fba6844553ee637
cebollinos11 <cebollinos@gmail.com>
2016-04-14 12:37:04 +0200

    level structure

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.anyKeyDown) {
+            Debug.Log("load first level");
+            Application.LoadLevel(1);
+        }
+	}
+}
