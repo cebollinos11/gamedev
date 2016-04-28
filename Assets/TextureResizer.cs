195444e9b7fe3b63a978062458bd8eaa6dd6ee87
cebollinos11 <cebollinos@gmail.com>
2016-04-28 12:18:59 +0200

    texture resizer in walls

---
+++ Assets/TextureResizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureResizer : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+         float multiplier = 0.5f;
+        //GetComponent<MeshRenderer>().material = material;
+        Vector2 scale = new Vector2(transform.lossyScale.x, transform.lossyScale.z) * multiplier;
+        GetComponent<MeshRenderer>().material.mainTextureScale = scale;
+        GetComponent<MeshRenderer>().material.SetTextureScale("_BumpMap", scale);
+	
+	}
+	
+	
+}
